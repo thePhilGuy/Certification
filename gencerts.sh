8e920a39181e025f5f7f142814bb623b8f4ec653
Philippe-Guillaume Losembe <pglosembe@gmail.com>
2016-02-29 12:29:26 -0500

    Script to generate certificates

---
+++ gencerts.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+TARGET=$1
+
+openssl genpkey -algorithm RSA -out $TARGET.key -pkeyopt rsa_keygen_bits:2048
+openssl req -x509 -sha256 -new -subj '/C=US/ST=NY/L=New York/CN=Netsec 4180' -key $TARGET.key -out $TARGET.csr
+openssl x509 -sha256 -days 365 -in $TARGET.csr -signkey $TARGET.key -out $TARGET.crt
